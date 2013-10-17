db8ab8d5122ec97d4d9bc697d030c7bd1d762b2a
Dominique DERRIER <derrierdo@gmail.com>
2013-10-16 21:13:39 -0400

    Version XML/Plex new framework

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,82 +1,105 @@
-# -*- coding: Latin-1 -*-
 import re
+BASE_URL = "http://webservice.canal-plus.com/rest/bigplayer/"
 
-"""
-"""
-
-PLUGIN_PREFIX           = "/video/Cplus"
-PLUGIN_ID               = "com.plexapp.plugins.Cplus"
+PLUGIN_PREFIX           = "/video/NCplus"
+PLUGIN_ID               = "com.plexapp.plugins.CNplus"
 PLUGIN_REVISION         = 0.1
 PLUGIN_UPDATES_ENABLED  = True
 CACHE_INTERVAL = 3600 * 2
 ICON="icon-default.png"
 ART= "bg-default.jpg"
-NAME="CPlus"
+NAME="Canal +"
 
-####################################################################################################
+
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu,NAME,R(ICON),R(ART))
-  Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
-  Plugin.AddViewGroup("ShowList", viewMode="List", mediaType="items")
-  MediaContainer.title1 = 'Cplus'
-  MediaContainer.content = 'Items'
-  MediaContainer.art = R(ART)
-  HTTP.SetCacheTime(CACHE_INTERVAL)
-
-####################################################################################################
-def MainMenu():
-  dir = MediaContainer(title1="Canal Plus", content = 'Items', art = R(ART))
-  dir.viewGroup = 'ShowList'
-  dir.Append(Function(DirectoryItem(sub_category, title="Le Zapping" , thumb=R("LeZapping.jpg")), Emission_cat='1830', stitle='zapping'))
-  dir.Append(Function(DirectoryItem(sub_category, title="Les Guignols" , thumb=R("LesGuignols.jpg")), Emission_cat='1784', stitle='guignols'))
-  dir.Append(Function(DirectoryItem(sub_category, title="Le Petit Journal" , thumb=R("LePetitJournal.jpg")), Emission_cat='3351', stitle='le-petit-journal'))
-  dir.Append(Function(DirectoryItem(sub_category, title="GroLand" , thumb=R("GroLand.jpg")), Emission_cat='1787', stitle='groland.con'))
-  dir.Append(Function(DirectoryItem(sub_category, title="Salut Les Terriens" , thumb=R("icon-default.png")), Emission_cat='3350', stitle='terriens'))
-  dir.Append(Function(DirectoryItem(sub_category, title="Effet Papillon" , thumb=R("icon-default.png")), Emission_cat='3356', stitle='papillon'))
-  return dir
-
-
-####################################################################################################
-def sub_category (sender, Emission_cat, title = None, replaceParent=False, values=None,stitle=False):
-	dir = MediaContainer(title1="", title2=title, replaceParent=replaceParent)
-	dir.viewGroup = 'Details'
-
-	mod=stitle
-	idd=Emission_cat
+	Plugin.AddPrefixHandler("/video/canalplus", ListeCategories, "Canal Plus", "icon-default.png", "art-default.jpg")
+	ObjectContainer.title1    = 'Canal Plus'
+	ObjectContainer.art       = R(ART)
 	
-	site_url='http://www.canalplus.fr/c/pid'+idd
-	fhtml=HTML.ElementFromURL(site_url)
-	string= XML.StringFromElement(fhtml)
-	lst= re.findall('like.*vid=([0-9]*)',string)
-	Evnt= max(lst)
 
-	base_address = 'http://service.canal-plus.com/video/rest/getVideosLiees/cplus/'+Evnt
-	data=XML.ElementFromURL(base_address)
-	hack="?secret=pqzerjlsmdkjfoiuerhsdlfknaes"
-	for video in data.xpath('//VIDEO'):
-		id= video.xpath('ID')[0].text
-		rub= video.xpath('RUBRIQUAGE/RUBRIQUE')[0].text
-		cat= video.xpath('RUBRIQUAGE/CATEGORIE')[0].text
-		dat= video.xpath('INFOS/PUBLICATION/DATE')[0].text
-		url= video.xpath('MEDIA/VIDEOS/HD')[0].text
-		img= video.xpath('MEDIA/IMAGES/GRAND')[0].text
-		com= video.xpath('INFOS/TITRAGE/TITRE')[0].text
-		com= com+ " "+video.xpath('INFOS/TITRAGE/SOUS_TITRE')[0].text
-		url=url.replace('rtmp://vod-fms.canalplus.fr/ondemand/videos','http://vod-flash.canalplus.fr/WWWPLUS/STREAMING')+hack
-		dir.Append(VideoItem(url,rub+" "+cat,'',com,'',img))
- 	return dir
+
+#Root categories
+def ListeCategories():
+	oc = ObjectContainer()
+
+	icon=None
+	idCategorie="39"
+	nom="X"
+#	oc.Append(Function(DirectoryItem(ListeVideos, title=nom, thumb=icon),idSousCategorie=idCategorie, nomSousCategorie="ZAPPING"))
+#	oc.Append(Function(DirectoryItem(ListeVideos, title=nom, thumb=icon),idSousCategorie=idCategorie, nomSousCategorie="LES_GUIGNOLS"))
+	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = idCategorie,nomSousCategorie="ZAPPING"),title="Zapping",thumb=R("LeZapping.jpg")))
+	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = idCategorie,nomSousCategorie="LES_GUIGNOLS"),title="Les guignols",thumb=R("LesGuignols.jpg")))
+	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = idCategorie,nomSousCategorie="LE_PETIT_JOURNAL"),title="Le petit Journal",thumb=R("LePetitJournal.jpg")))
+	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "1080",nomSousCategorie="GROLAND_EMISSIONS"),title="GroLand",thumb=R("GroLand.jpg")))
+	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "1080",nomSousCategorie="SALUT_LES_TERRIENS"),title="Slt",thumb=R("cplus-default.jpg")))
+	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "1080",nomSousCategorie="LES_NOUVEAUX_EXPLORATEURS"),title="Explorateur",thumb=R("cplus-default.jpg")))
+	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "1080",nomSousCategorie="LE_SUPPLEMENT"),title="Le supplement",thumb=R("cplus-default.jpg")))
+	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "1080",nomSousCategorie="L_EFFET_PAPILLON"),title="L'effet papillon",thumb=R("cplus-default.jpg")))
+	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "105",nomSousCategorie="L_OEIL_DE_LINKS"),title="Oeil de links",thumb=R("cplus-default.jpg")))
+	return oc
+
+
+#Chosen sub-category's videos
+def ListeVideos(idSousCategorie, nomSousCategorie):
+	oc =ObjectContainer(title1="", title2=nomSousCategorie)
+	dirvideos = XML.ElementFromURL(BASE_URL + "getMEAs/" + idSousCategorie).xpath("//MEA[RUBRIQUAGE/RUBRIQUE='%s']"%(nomSousCategorie))
+#	for listv in dirvideos:
+
+	tb={}
+	for listv in dirvideos:
+		idVideo = listv.xpath('./ID')[0].text
+	        videosXml = XML.ElementFromURL(BASE_URL + "getVideosLiees/" + idVideo)
+		videos = videosXml.xpath("//VIDEO")
+		hack="?secret=pqzerjlsmdkjfoiuerhsdlfknaes"
+		for video in videos:
+			idv=video.xpath('./ID')[0].text
+			titre = video.xpath('./INFOS/TITRAGE/TITRE')[0].text
+			soustitre = video.xpath('./INFOS/TITRAGE/SOUS_TITRE')[0].text
+			if soustitre.strip() != "":
+				titre =  titre + " - " + soustitre
+			description = video.xpath('./INFOS/DESCRIPTION')[0].text
+			thumb = video.xpath('.//MEDIA/IMAGES/GRAND')[0].text
+			video_url = video.xpath('.//MEDIA/VIDEOS/HD')[0].text
+			#"&%s&%s&%s"%(titre,thumb,description)
+			video_url=video_url.replace('rtmp://vod-fms.canalplus.fr/ondemand/videos','http://vod-flash.canalplus.fr/WWWPLUS/STREAMING')
+			video_url=video_url.replace('rtmp://geo2-vod-fms.canalplus.fr/ondemand/geo2','http://vod-flash.canalplus.fr/WWWPLUS/STREAMING')
+			video_url=video_url+hack
+
+			dd={'thumb':thumb,'summary':description,'titre':titre}
+			tb[idv]={'video_url':video_url+"&"+JSON.StringFromObject(dd),'thumb':thumb,'description':description,'titre':titre}
+	for idv in sorted(tb,reverse=True):
 		
-#  xml_sections = ETXML.parse(urllib.urlopen(base_address))
-#  sections = xml_sections.getroot()
-#  for s in range(0,len(sections)):
-#  	rub= sections[s].find('RUBRIQUAGE').find('RUBRIQUE').text
-#  	cat= sections[s].find('RUBRIQUAGE').find('CATEGORIE').text
-#  	inf= sections[s].find('INFOS').find('PUBLICATION').find('DATE').text
-#  	url= sections[s].find('MEDIA').find('VIDEOS').find('HD').text
-#  	img= sections[s].find('MEDIA').find('IMAGES').find('PETIT').text
-#	com= sections[s].find('INFOS').find('TITRAGE').find('SOUS_TITRE').text
-##Replace 
-# rtmp://vod-fms.canalplus.fr/ondemand/videos
-#by http://vod-flash.canalplus.fr/WWWPLUS/STREAMING
-#	dir.Append(VideoItem(url,rub+" "+cat,'',rub+"\n"+inf+"\n"+com,'',img))
-####################################################################################################
+		dd=MovieObject(url=tb[idv]['video_url'],title=tb[idv]['titre'],summary=tb[idv]['description'],thumb=tb[idv]['thumb'])
+		oc.add(dd)
+	
+	return oc
+
+#Video chosen + related videos
+def ListeVideosLiees(sender,idVideo, nomSousCategorie):
+	oc = ObjectContainer(title2 = "video", art = None)
+#	dir=MediaContainer(title1="", title2="Video", replaceParent=None)
+
+	
+	videosXml = XML.ElementFromURL(BASE_URL + "getVideosLiees/" + idVideo)
+	videos = videosXml.xpath("//VIDEO[ID='"+idVideo+"']")
+	hack="?secret=pqzerjlsmdkjfoiuerhsdlfknaes"	
+	videos.extend(videosXml.xpath("//VIDEO[ID!='"+idVideo+"']"))
+	for video in videos:
+		titre = video.xpath('./INFOS/TITRAGE/TITRE')[0].text
+		soustitre = video.xpath('./INFOS/TITRAGE/SOUS_TITRE')[0].text
+		if soustitre.strip() != "":
+			titre =  titre + " - " + soustitre
+
+		description = video.xpath('./INFOS/DESCRIPTION')[0].text
+		thumb = video.xpath('.//MEDIA/IMAGES/GRAND')[0].text
+		video_url = video.xpath('.//MEDIA/VIDEOS/HD/text()')[0]
+		#video_url = video.xpath('.//URL/text()')[0]
+		#oc.add(VideoItem(video_url,titre,"",description,thumb))
+		
+		#MediaObjectsForURL(video_url)
+		dd=MovieObject(url=video_url,title=titre,summary=description,thumb=thumb)
+		oc.add(dd)
+		#	oc.add(MovieObject(url=video_url, title=titre, summary=description,
+		#	thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback="icon-default.png")))
+		#	dir.Append(VideoItem(video_url,titre,'',description,'',thumb))
+	return oc
