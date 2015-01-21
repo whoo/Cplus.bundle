b41affb41896cb9d26c94ce898d587b87716bc1c
Dominique DERRIER <you@example.com>
2015-01-20 20:49:04 -0500

    Add duration control

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -22,23 +22,35 @@ def Start():
 def ListeCategories():
 	oc = ObjectContainer()
 
+        ListVids=[
+        {'duree': 3,'cat':39,'nom':'ZAPPING','title':'Zapping','icon':'LeZapping.jpg'},
+        {'duree': 5,'cat':39,'nom':'LES_GUIGNOLS','title':'Les Guignols de l\'info','icon':'LesGuignols.jpg'},
+        {'duree': 10,'cat':39,'nom':'LE_PETIT_JOURNAL','title':'Le Petit Journal','icon':'LePetitJournal.jpg'},
+        {'duree': 10,'cat':39,'nom':'LE_GRAND_JOURNAL','title':'Le Grand Journal','icon':'LeGrandJournal.jpg'},
+        {'duree': 10,'cat':39,'nom':'LE.JT.DE.CANAL','title':'Le Journal de canal','icon':'jtcanal.jpg'},
+        {'duree': 10,'cat':1080,'nom':'SALUT.LES.TERRIENS','title':'Salut les Terriens','icon':'Slt.jpg'},
+        {'duree': 10,'cat':1080,'nom':'LES_NOUVEAUX_EXPLORATEURS','title':'Les Explorateurs','icon':'Explorateurs.jpg'},
+        {'duree': 10,'cat':130,'nom':'GROLAND','title':'GroLand','icon':'GroLand.jpg'},
+        {'duree': 10,'cat':1080,'nom':'LE_SUPPLEMENT','title':'Le supplement politique','icon':'supplementpolitique.jpg'},
+        {'duree': 10,'cat':105,'nom':'L_OEIL_DE_LINKS','title':'Oeil de links','icon':'oeildelinks.jpg'}
+        ]
+
+
 	icon=None
-	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "39",nomSousCategorie="ZAPPING"),title="Zapping",thumb=R("LeZapping.jpg")))
-	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "39",nomSousCategorie="LES_GUIGNOLS"),title="Les guignols",thumb=R("LesGuignols.jpg")))
-	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "39",nomSousCategorie="LE_PETIT_JOURNAL"),title="Le petit Journal",thumb=R("LePetitJournal.jpg")))
-	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "39",nomSousCategorie="LE_GRAND_JOURNAL"),title="Le Grand Journal",thumb=R("LeGrandJournal.jpg")))
-	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "39",nomSousCategorie="LE.JT.DE.CANAL."),title="Le JT",thumb=R("jtcanal.jpg")))
-	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "130",nomSousCategorie="GROLAND"),title="GroLand",thumb=R("GroLand.jpg")))
-	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "1080",nomSousCategorie="SALUT.LES.TERRIENS"),title="Salut Les Terriens",thumb=R("Slt.jpg")))
-	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "1080",nomSousCategorie="LES_NOUVEAUX_EXPLORATEURS"),title="Les nouveaux Explorateurs",thumb=R("Explorateurs.jpg")))
-	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "1080",nomSousCategorie="LE_SUPPLEMENT"),title="Le supplement politique",thumb=R("supplementpolitique.jpg")))
-	oc.add(DirectoryObject(key=Callback(ListeVideos,idSousCategorie = "105",nomSousCategorie="L_OEIL_DE_LINKS"),title="Oeil de links",thumb=R("oeildelinks.jpg")))
+        for a in ListVids:
+            oc.add(DirectoryObject(key=Callback(ListeVideos,Video=a),title=a['title'],thumb=R(a['icon'])))
+
+
 	return oc
 
 
 #Chosen sub-category's videos
-def ListeVideos(idSousCategorie, nomSousCategorie):
-	oc =ObjectContainer(title1="", title2=nomSousCategorie)
+def ListeVideos(Video):
+        """List Video from main categorie"""
+        idSousCategorie=str(Video['cat'])
+        nomSousCategorie=Video['nom']
+
+	oc =ObjectContainer(title1="", title2=Video['title'])
 	regexpNS = "http://exslt.org/regular-expressions"
 	dirvideos = XML.ElementFromURL(BASE_URL + "getMEAs/cplus/" + idSousCategorie).xpath("//MEA[RUBRIQUAGE/*[re:test(., '%s', 'i')]]"%(nomSousCategorie),namespaces={'re':regexpNS})
 
@@ -54,18 +66,19 @@ def ListeVideos(idSousCategorie, nomSousCategorie):
 			soustitre = video.xpath('./INFOS/TITRAGE/SOUS_TITRE')[0].text
                         duration = video.xpath('./DURATION')[0].text
                         if (duration is None):
-                            duration=10
+                            duration=60*6
 			if soustitre.strip() != "":
 				titre =  titre + " - " + soustitre 
                         description = soustitre+ " ("+str(int(duration)/60)+"min.)" + "\n" + video.xpath('./INFOS/DESCRIPTION')[0].text.replace('"','\'')
 			thumb = video.xpath('.//MEDIA/IMAGES/GRAND')[0].text
 			video_url = video.xpath('.//MEDIA/VIDEOS/HD')[0].text
-			if (video_url != None ):
-				video_url=video_url.replace('rtmp://vod-fms.canalplus.fr/ondemand/videos','http://vod-flash.canalplus.fr/WWWPLUS/STREAMING')
-				video_url=video_url.replace('rtmp://ugc-vod-fms.canalplus.fr/ondemand/videos','http://vod-flash.canalplus.fr/WWWPLUS/STREAMING')	
-				video_url=video_url+hack
-				dd={'thumb':thumb,'summary':description,'titre':titre}
-				tb[idv]={'video_url':video_url+"&"+JSON.StringFromObject(dd),'thumb':thumb,'description':description,'titre':titre}
+                        if ( int(duration)/60 >Video['duree']):
+        			if (video_url != None ):
+        				video_url=video_url.replace('rtmp://vod-fms.canalplus.fr/ondemand/videos','http://vod-flash.canalplus.fr/WWWPLUS/STREAMING')
+        				video_url=video_url.replace('rtmp://ugc-vod-fms.canalplus.fr/ondemand/videos','http://vod-flash.canalplus.fr/WWWPLUS/STREAMING')	
+        				video_url=video_url+hack
+        				dd={'thumb':thumb,'summary':description,'titre':titre}
+        				tb[idv]={'video_url':video_url+"&"+JSON.StringFromObject(dd),'thumb':thumb,'description':description,'titre':titre}
 
 	for idv in sorted(tb,reverse=True,key=int):
 		dd=MovieObject(url=tb[idv]['video_url'],title=tb[idv]['titre'],summary=tb[idv]['description'],thumb=tb[idv]['thumb'])
